334020970e1e0141fad6b2f972902878120d690d
David Palm <dvdplm@gmail.com>
2018-08-22 14:20:25 +0200

    Use new uint version BREAKING CHANGE: Remove U1024 and H1024 Remove conversion facilities that moved to uint Remove asm Cleanup

--- ethereum-types/src/hash.rs
+++ ethereum-types/src/hash.rs
@@ -1,4 +1,4 @@
-use {U64, U128, U256, U512, U1024};
+use {U64, U128, U256, U512};
 
 #[cfg(feature="serialize")]
 use serde::{Serialize, Serializer, Deserialize, Deserializer};
@@ -67,7 +67,6 @@ impl_serde!(H256, 32);
 impl_serde!(H264, 33);
 impl_serde!(H512, 64);
 impl_serde!(H520, 65);
-impl_serde!(H1024, 128);
 
 construct_hash!(H32, 4);
 construct_hash!(H64, 8);
@@ -77,13 +76,11 @@ construct_hash!(H256, 32);
 construct_hash!(H264, 33);
 construct_hash!(H512, 64);
 construct_hash!(H520, 65);
-construct_hash!(H1024, 128);
 
 impl_uint_conversions!(H64, U64);
 impl_uint_conversions!(H128, U128);
 impl_uint_conversions!(H256, U256);
 impl_uint_conversions!(H512, U512);
-impl_uint_conversions!(H1024, U1024);
 
 #[deprecated]
 impl From<H256> for H160 {
--- ethereum-types/src/lib.rs
+++ ethereum-types/src/lib.rs
@@ -1,7 +1,5 @@
 #![cfg_attr(not(feature="std"), no_std)]
 
-#![cfg_attr(asm_available, feature(asm))]
-
 #[cfg(feature="std")]
 extern crate core;
 #[macro_use]
@@ -23,8 +21,8 @@ extern crate serde_json;
 mod hash;
 mod uint;
 
-pub use uint::{U64, U128, U256, U512, U1024};
-pub use hash::{H32, H64, H128, H160, H256, H264, H512, H520, H1024};
+pub use uint::{U64, U128, U256, U512};
+pub use hash::{H32, H64, H128, H160, H256, H264, H512, H520};
 pub use ethbloom::{Bloom, BloomRef, Input as BloomInput};
 pub use fixed_hash::clean_0x;
 
