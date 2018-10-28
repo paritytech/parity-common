351dc7f6c641c3ae56593b57b4286458dd0e7158
Herobird <robbepop@web.de>
2018-10-28 20:42:43 +0100

    Inner field of Hash tuple struct is no longer publicly visible

--- fixed-hash/src/hash.rs
+++ fixed-hash/src/hash.rs
@@ -51,7 +51,7 @@ macro_rules! construct_hash {
 	( $(#[$attr:meta])* $visibility:vis struct $name:ident ( $n_bytes:expr ); ) => {
 		#[repr(C)]
 		$(#[$attr])*
-		$visibility struct $name (pub [u8; $n_bytes]);
+		$visibility struct $name ([u8; $n_bytes]);
 
 		impl From<[u8; $n_bytes]> for $name {
 			#[inline]
