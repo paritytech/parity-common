8a06e416974af780731bb2023bdccce01fae348f
Wei Tang <hi@that.world>
2018-12-04 11:43:38 +0800

    Fix style nit

--- primitive-types/src/lib.rs
+++ primitive-types/src/lib.rs
@@ -36,9 +36,12 @@ extern crate impl_codec;
 extern crate impl_rlp;
 
 construct_uint!(U256, 4);
-#[cfg(feature = "impl-serde")] impl_uint_serde!(U256, 4);
-#[cfg(feature = "impl-codec")] impl_uint_codec!(U256, 4);
-#[cfg(feature = "impl-rlp")] impl_uint_rlp!(U256, 4);
+#[cfg(feature = "impl-serde")]
+impl_uint_serde!(U256, 4);
+#[cfg(feature = "impl-codec")]
+impl_uint_codec!(U256, 4);
+#[cfg(feature = "impl-rlp")]
+impl_uint_rlp!(U256, 4);
 
 impl U256 {
 	/// Multiplies two 256-bit integers to produce full 512-bit integer
@@ -104,17 +107,23 @@ impl<'a> From<&'a U512> for U256 {
 }
 
 construct_uint!(U512, 8);
-#[cfg(feature = "impl-serde")] impl_uint_serde!(U512, 8);
-#[cfg(feature = "impl-codec")] impl_uint_codec!(U512, 8);
-#[cfg(feature = "impl-rlp")] impl_uint_rlp!(U512, 8);
+#[cfg(feature = "impl-serde")]
+impl_uint_serde!(U512, 8);
+#[cfg(feature = "impl-codec")]
+impl_uint_codec!(U512, 8);
+#[cfg(feature = "impl-rlp")]
+impl_uint_rlp!(U512, 8);
 
 construct_fixed_hash! {
 	/// Fixed-size uninterpreted hash type with 20 bytes (160 bits) size.
 	pub struct H160(20);
 }
-#[cfg(feature = "impl-serde")] impl_fixed_hash_serde!(H160, 20);
-#[cfg(feature = "impl-codec")] impl_fixed_hash_codec!(H160, 20);
-#[cfg(feature = "impl-rlp")] impl_fixed_hash_rlp!(H160, 20);
+#[cfg(feature = "impl-serde")]
+impl_fixed_hash_serde!(H160, 20);
+#[cfg(feature = "impl-codec")]
+impl_fixed_hash_codec!(H160, 20);
+#[cfg(feature = "impl-rlp")]
+impl_fixed_hash_rlp!(H160, 20);
 
 #[cfg(feature = "std")]
 impl From<u64> for H160 {
@@ -143,9 +152,12 @@ construct_fixed_hash! {
 	/// Fixed-size uninterpreted hash type with 32 bytes (256 bits) size.
 	pub struct H256(32);
 }
-#[cfg(feature = "impl-serde")] impl_fixed_hash_serde!(H256, 32);
-#[cfg(feature = "impl-codec")] impl_fixed_hash_codec!(H256, 32);
-#[cfg(feature = "impl-rlp")] impl_fixed_hash_rlp!(H256, 32);
+#[cfg(feature = "impl-serde")]
+impl_fixed_hash_serde!(H256, 32);
+#[cfg(feature = "impl-codec")]
+impl_fixed_hash_codec!(H256, 32);
+#[cfg(feature = "impl-rlp")]
+impl_fixed_hash_rlp!(H256, 32);
 
 #[cfg(feature = "std")]
 impl From<u64> for H256 {
@@ -158,9 +170,12 @@ construct_fixed_hash! {
 	/// Fixed-size uninterpreted hash type with 64 bytes (512 bits) size.
 	pub struct H512(64);
 }
-#[cfg(feature = "impl-serde")] impl_fixed_hash_serde!(H512, 64);
-#[cfg(feature = "impl-codec")] impl_fixed_hash_codec!(H512, 64);
-#[cfg(feature = "impl-rlp")] impl_fixed_hash_rlp!(H512, 64);
+#[cfg(feature = "impl-serde")]
+impl_fixed_hash_serde!(H512, 64);
+#[cfg(feature = "impl-codec")]
+impl_fixed_hash_codec!(H512, 64);
+#[cfg(feature = "impl-rlp")]
+impl_fixed_hash_rlp!(H512, 64);
 
 #[cfg(feature = "std")]
 impl From<u64> for H512 {
