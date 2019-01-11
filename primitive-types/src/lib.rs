9eba22f43ddea225c02001ef0fd61254e0c92115
Tomasz Drwięga <tomasz@parity.io>
2019-01-11 14:31:55 +0100

    More compact.

--- primitive-types/src/lib.rs
+++ primitive-types/src/lib.rs
@@ -36,12 +36,10 @@ construct_uint! {
 	/// 128-bit unsigned integer.
 	pub struct U128(2);
 }
-
 construct_uint! {
 	/// 256-bit unsigned integer.
 	pub struct U256(4);
 }
-
 construct_uint! {
 	/// 512-bits unsigned integer.
 	pub struct U512(8);
