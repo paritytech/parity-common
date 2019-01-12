64135ac0aaf960f46ac0ee843984ae9067441e8a
Emeric Chevalier <emericchevalier.pro@gmail.com>
2019-01-12 17:43:16 +0100

    Use malloc_size_of_derive from crates.

--- parity-util-mem/get_malloc_size_src.sh
+++ parity-util-mem/get_malloc_size_src.sh
@@ -8,6 +8,5 @@ git checkout 5bdea7dc1c80790a852a3fb03edfb2b8fbd403dc
 git apply ../slim_malloc_size_of.patch
 #git merge master
 #cp components/malloc_size_of/lib.rs ../src/malloc_size.rs
-#cp -r components/malloc_size_of_derive ..
 #cd ..
 #rm -rf ./servo
