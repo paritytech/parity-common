85d6003a6717517a6362c20bc4030a4bda23d242
Andronik Ordian <write@reusable.software>
2019-04-11 12:22:40 +0200

    fix(rocksdb): cap block lru cache in 8 MB, not 8 bytes

--- kvdb-rocksdb/src/lib.rs
+++ kvdb-rocksdb/src/lib.rs
@@ -305,7 +305,7 @@ impl Database {
 
 		{
 			block_opts.set_block_size(config.compaction.block_size);
-			let cache_size = cmp::max(8, config.memory_budget() / 3);
+			let cache_size = cmp::max(8 * MB, config.memory_budget() / 3);
 			let cache = Cache::new(cache_size);
 			block_opts.set_cache(cache);
 		}
