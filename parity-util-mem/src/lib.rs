2a8a33a641135eae78d1f3f51c80802c394c3bf6
Andronik Ordian <write@reusable.software>
2019-07-01 17:46:02 +0200

    [parity-util-mem] remove Memzero (#183)

--- parity-util-mem/src/lib.rs
+++ parity-util-mem/src/lib.rs
@@ -31,9 +31,6 @@ extern crate alloc;
 
 extern crate malloc_size_of_derive as malloc_size_derive;
 
-use std::ops::{Deref, DerefMut};
-
-use std::ptr;
 
 cfg_if! {
 	if #[cfg(all(
@@ -90,42 +87,6 @@ pub use malloc_size::{
 	MallocSizeOf,
 };
 pub use allocators::MallocSizeOfExt;
-
-/// Wrapper to zero out memory when dropped.
-#[derive(Clone, PartialEq, Eq, PartialOrd, Ord, Hash)]
-pub struct Memzero<T: AsMut<[u8]>> {
-	mem: T,
-}
-
-impl<T: AsMut<[u8]>> From<T> for Memzero<T> {
-	fn from(mem: T) -> Memzero<T> {
-		Memzero { mem }
-	}
-}
-
-impl<T: AsMut<[u8]>> Drop for Memzero<T> {
-	fn drop(&mut self) {
-		unsafe {
-			for byte_ref in self.mem.as_mut() {
-				ptr::write_volatile(byte_ref, 0)
-			}
-		}
-	}
-}
-
-impl<T: AsMut<[u8]>> Deref for Memzero<T> {
-	type Target = T;
-
-	fn deref(&self) -> &Self::Target {
-		&self.mem
-	}
-}
-
-impl<T: AsMut<[u8]>> DerefMut for Memzero<T> {
-	fn deref_mut(&mut self) -> &mut Self::Target {
-		&mut self.mem
-	}
-}
 
 #[cfg(feature = "std")]
 #[cfg(test)]
