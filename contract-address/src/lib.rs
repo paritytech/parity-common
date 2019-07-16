14a7c734b040a755cffabba1d4a1c6c22e099ae5
Andronik Ordian <write@reusable.software>
2019-07-16 15:25:31 +0200

    [contract-address] extract into a crate

---
+++ contract-address/src/lib.rs
@@ -0,0 +1,79 @@
+// Copyright 2019 Parity Technologies (UK) Ltd.
+// This file is part of Parity Ethereum.
+
+// Parity Ethereum is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+
+// Parity Ethereum is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License
+// along with Parity Ethereum.  If not, see <http://www.gnu.org/licenses/>.
+
+//! A utility to create an ethereum contract address.
+
+pub use ethereum_types::{Address, H256, U256};
+use keccak_hash::keccak;
+use rlp::RlpStream;
+
+/// Specifies how an address is calculated for a new contract.
+#[derive(Copy, Clone, PartialEq, Eq, Debug, Hash)]
+pub enum CreateContractAddress {
+	/// Address is calculated from sender and nonce. pWASM `create` scheme.
+	FromSenderAndNonce,
+	/// Address is calculated from sender, salt and code hash. pWASM `create2` scheme and EIP-1014 CREATE2 scheme.
+	FromSenderSaltAndCodeHash(H256),
+	/// Address is calculated from code hash and sender. Used by pwasm create ext.
+	FromSenderAndCodeHash,
+}
+
+/// Returns new address created from address, nonce, and code hash
+pub fn contract_address(
+	address_scheme: CreateContractAddress,
+	sender: &Address,
+	nonce: &U256,
+	code: &[u8],
+) -> (Address, Option<H256>) {
+	match address_scheme {
+		CreateContractAddress::FromSenderAndNonce => {
+			let mut stream = RlpStream::new_list(2);
+			stream.append(sender);
+			stream.append(nonce);
+			(From::from(keccak(stream.as_raw())), None)
+		},
+		CreateContractAddress::FromSenderSaltAndCodeHash(salt) => {
+			let code_hash = keccak(code);
+			let mut buffer = [0u8; 1 + 20 + 32 + 32];
+			buffer[0] = 0xff;
+			&mut buffer[1..(1+20)].copy_from_slice(&sender[..]);
+			&mut buffer[(1+20)..(1+20+32)].copy_from_slice(&salt[..]);
+			&mut buffer[(1+20+32)..].copy_from_slice(&code_hash[..]);
+			(From::from(keccak(&buffer[..])), Some(code_hash))
+		},
+		CreateContractAddress::FromSenderAndCodeHash => {
+			let code_hash = keccak(code);
+			let mut buffer = [0u8; 20 + 32];
+			&mut buffer[..20].copy_from_slice(&sender[..]);
+			&mut buffer[20..].copy_from_slice(&code_hash[..]);
+			(From::from(keccak(&buffer[..])), Some(code_hash))
+		},
+	}
+}
+
+#[cfg(test)]
+mod tests {
+	use super::*;
+	use std::str::FromStr;
+
+	#[test]
+	fn test_from_sender_and_nonce() {
+		let sender = Address::from_str("0f572e5295c57f15886f9b263e2f6d2d6c7b5ec6").unwrap();
+		let expected = Address::from_str("3f09c73a5ed19289fb9bdc72f1742566df146f56").unwrap();
+		let scheme = CreateContractAddress::FromSenderAndNonce;
+		assert_eq!(expected, contract_address(scheme, &sender, &U256::from(88), &[]).0);
+	}
+}
