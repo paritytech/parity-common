c70b31d70dfec0187f1cfd74e5218cb54b7b3729
Andronik Ordian <write@reusable.software>
2019-09-24 22:43:24 +0200

    grammar pass by @dvdplm

--- kvdb-web/src/lib.rs
+++ kvdb-web/src/lib.rs
@@ -71,7 +71,7 @@ impl Database {
 			// If we need more column than the latest version has,
 			// then bump the version (+ 1 for the default column).
 			// In order to bump the version, we close the database
-			// and reopen it with higher version that it was opened previously.
+			// and reopen it with a higher version than it was opened with previously.
 			// cf. https://github.com/paritytech/parity-common/pull/202#discussion_r321221751
 			if columns + 1 > db.columns {
 				let next_version = db.version + 1;
