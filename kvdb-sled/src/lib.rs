a482aaccc71fa2d0d904897a828457bcb3502adc
Andronik Ordian <write@reusable.software>
2019-10-29 15:55:38 +0100

    sled: tests from rocksdb

--- kvdb-sled/src/lib.rs
+++ kvdb-sled/src/lib.rs
@@ -30,46 +30,84 @@ fn other_io_err<E>(e: E) -> io::Error where E: Into<Box<dyn std::error::Error + 
 }
 
 pub struct Database {
-	// FIXME: sled currently support transactions only on tuples of trees,
-	// see https://github.com/spacejam/sled/issues/382#issuecomment-526548082
+	db: sled::Db,
 	// `sled::Tree` corresponds to a `Column` in the KeyValueDB terminology.
 	columns: Vec<sled::Tree>,
 	path: String,
-	num_columns: u8,
 }
 
 // TODO: docs
 pub struct DatabaseConfig {
 	pub columns: Option<u8>,
-	pub memory_budget_mb: Option<u64>,
 	pub path: String,
+	pub memory_budget_mb: Option<u64>,
 }
 
 impl DatabaseConfig {
+	pub fn new(path: String, columns: Option<u8>) -> Self {
+		Self {
+			columns,
+			path,
+			memory_budget_mb: None,
+		}
+	}
 	pub fn memory_budget(&self) -> u64 {
 		self.memory_budget_mb.unwrap_or(DB_DEFAULT_MEMORY_BUDGET_MB) * MB
 	}
 }
 
+fn to_sled_config(config: &DatabaseConfig) -> sled::Config {
+	let conf = sled::Config::default()
+		.path(&config.path)
+		.cache_capacity(config.memory_budget() / 2)
+		.flush_every_ms(Some(2_000)); // TODO: a random constant
+	// .snapshot_after_ops(100_000);
+	conf
+}
+
+fn col_name(col: u8) -> String {
+	format!("col{}", col)
+}
+
 impl Database {
-	pub fn open(config: DatabaseConfig) -> sled::Result<Database> {
-		let conf = sled::Config::default()
-			.path(&config.path)
-			.cache_capacity(config.memory_budget() / 2)
-			.flush_every_ms(Some(2_000)); // TODO: a random constant
-			// .snapshot_after_ops(100_000);
+	pub fn open(config: &DatabaseConfig) -> sled::Result<Database> {
+		let conf = to_sled_config(config);
 
 		let db = conf.open()?;
-		let num_columns = config.columns.map_or(0, |c| c + 1);
-		let columns = (0..=num_columns)
-			.map(|i| db.open_tree(format!("col{}", i).as_bytes()))
+		let num_columns = config.columns.map_or(1, |c| c + 1);
+		let columns = (0..num_columns)
+			.map(|i| db.open_tree(col_name(i).as_bytes()))
 			.collect::<sled::Result<Vec<_>>>()?;
 
 		Ok(Database {
+			db,
 			columns,
-			path: config.path,
-			num_columns,
+			path: config.path.clone(),
 		})
+	}
+
+	/// The number of non-default column families.
+	pub fn num_columns(&self) -> u8 {
+		self.columns.len() as u8 - 1
+	}
+
+	/// Drop a column family.
+	pub fn drop_column(&mut self) -> io::Result<()> {
+		if let Some(col) = self.columns.pop() {
+			let name = col_name(self.columns.len() as u8);
+			drop(col);
+			self.db.drop_tree(name.as_bytes()).map_err(other_io_err)?;
+		}
+		Ok(())
+	}
+
+	/// Add a column family.
+	pub fn add_column(&mut self) -> io::Result<()> {
+		let col = self.columns.len() as u8;
+		let name = col_name(col);
+		let tree = self.db.open_tree(name.as_bytes()).map_err(other_io_err)?;
+		self.columns.push(tree);
+		Ok(())
 	}
 
 	fn to_sled_column(col: Option<u32>) -> u8 {
@@ -105,7 +143,7 @@ impl KeyValueDB for Database {
 	}
 
 	fn write_buffered(&self, tr: DBTransaction) {
-		// REVIEW: not sure if it's correct semantically
+		// REVIEW: not sure if it's semantically correct
 		//         to apply an ACID transaction here
 		let result = self.write(tr);
 		if result.is_err() {
@@ -114,6 +152,8 @@ impl KeyValueDB for Database {
 	}
 
 	fn write(&self, tr: DBTransaction) -> io::Result<()> {
+		// FIXME: sled currently support transactions only on tuples of trees,
+		// see https://github.com/spacejam/sled/issues/382#issuecomment-526548082
 		// TODO: implement for more sizes via macro
 		let result = match &self.columns[..] {
 			[c1] => c1.transaction(|c1| {
@@ -209,8 +249,126 @@ impl Drop for Database {
 
 #[cfg(test)]
 mod tests {
+	use tempdir::TempDir;
+	use super::*;
+
 	#[test]
-	fn it_works() {
-		assert_eq!(2 + 2, 4);
+	fn add_columns() {
+		let tempdir = TempDir::new("sled-test-add_columns").unwrap().path().to_str().unwrap().to_owned();
+
+		// open empty, add 5.
+		{
+			let config = DatabaseConfig::new(tempdir.clone(), None);
+			let mut db = Database::open(&config).unwrap();
+			assert_eq!(db.num_columns(), 0);
+
+			for i in 0..5 {
+				db.add_column().unwrap();
+				assert_eq!(db.num_columns(), i + 1);
+			}
+		}
+
+		// reopen as 5.
+		{
+			let config_5 = DatabaseConfig::new(tempdir.to_owned(), Some(5));
+			let db = Database::open(&config_5).unwrap();
+			assert_eq!(db.num_columns(), 5);
+		}
+	}
+
+	#[test]
+	fn drop_columns() {
+		let tempdir = TempDir::new("sled-test-drop_columns").unwrap().path().to_str().unwrap().to_owned();
+
+		// open 5, remove all.
+		{
+			let config_5 = DatabaseConfig::new(tempdir.clone(), Some(5));
+			let mut db = Database::open(&config_5).unwrap();
+			assert_eq!(db.num_columns(), 5);
+
+			for i in (0..5).rev() {
+				db.drop_column().unwrap();
+				assert_eq!(db.num_columns(), i);
+			}
+		}
+
+		// reopen as 0.
+		{
+			let config = DatabaseConfig::new(tempdir, None);
+			let db = Database::open(&config).unwrap();
+			assert_eq!(db.num_columns(), 0);
+		}
+	}
+
+	#[test]
+	fn write_clears_buffered_ops() {
+		let tempdir = TempDir::new("sled-test-write_clears_buffered_ops").unwrap().path().to_str().unwrap().to_owned();
+		let config = DatabaseConfig::new(tempdir, None);
+		let db = Database::open(&config).unwrap();
+
+		let mut batch = db.transaction();
+		batch.put(None, b"foo", b"bar");
+		db.write_buffered(batch);
+
+		let mut batch = db.transaction();
+		batch.put(None, b"foo", b"baz");
+		db.write(batch).unwrap();
+
+		assert_eq!(db.get(None, b"foo").unwrap().unwrap().as_ref(), b"baz");
+	}
+
+	#[test]
+	fn test_db() {
+		let tempdir = TempDir::new("sled-test-write_clears_buffered_ops").unwrap().path().to_str().unwrap().to_owned();
+		let config = DatabaseConfig::new(tempdir, None);
+		let db = Database::open(&config).unwrap();
+		let key1 = b"02c69be41d0b7e40352fc85be1cd65eb03d40ef8427a0ca4596b1ead9a00e9fc";
+		let key2 = b"03c69be41d0b7e40352fc85be1cd65eb03d40ef8427a0ca4596b1ead9a00e9fc";
+		let key3 = b"01c69be41d0b7e40352fc85be1cd65eb03d40ef8427a0ca4596b1ead9a00e9fc";
+
+		let mut batch = db.transaction();
+		batch.put(None, key1, b"cat");
+		batch.put(None, key2, b"dog");
+		db.write(batch).unwrap();
+
+		assert_eq!(&*db.get(None, key1).unwrap().unwrap(), b"cat");
+
+		let contents: Vec<_> = db.iter(None).into_iter().collect();
+		assert_eq!(contents.len(), 2);
+		assert!(contents[0].0.to_vec() == key1.to_vec());
+		assert_eq!(&*contents[0].1, b"cat");
+		assert_eq!(contents[1].0.to_vec(), key2.to_vec());
+		assert_eq!(&*contents[1].1, b"dog");
+
+		let mut batch = db.transaction();
+		batch.delete(None, key1);
+		db.write(batch).unwrap();
+
+		assert!(db.get(None, key1).unwrap().is_none());
+
+		let mut batch = db.transaction();
+		batch.put(None, key1, b"cat");
+		db.write(batch).unwrap();
+
+		let mut transaction = db.transaction();
+		transaction.put(None, key3, b"elephant");
+		transaction.delete(None, key1);
+		db.write(transaction).unwrap();
+		assert!(db.get(None, key1).unwrap().is_none());
+		assert_eq!(&*db.get(None, key3).unwrap().unwrap(), b"elephant");
+
+		assert_eq!(&*db.get_by_prefix(None, key3).unwrap(), b"elephant");
+		assert_eq!(&*db.get_by_prefix(None, key2).unwrap(), b"dog");
+
+		let mut transaction = db.transaction();
+		transaction.put(None, key1, b"horse");
+		transaction.delete(None, key3);
+		db.write_buffered(transaction);
+		assert!(db.get(None, key3).unwrap().is_none());
+		assert_eq!(&*db.get(None, key1).unwrap().unwrap(), b"horse");
+
+		db.flush().unwrap();
+		assert!(db.get(None, key3).unwrap().is_none());
+		assert_eq!(&*db.get(None, key1).unwrap().unwrap(), b"horse");
 	}
 }
