04dcab45d7d488d26c56069892ff0a92165579bd
NikVolf <nikvolf@gmail.com>
2019-12-21 19:33:50 +0300

    add another test

--- parity-util-mem/tests/derive.rs
+++ parity-util-mem/tests/derive.rs
@@ -13,3 +13,20 @@ fn derive_smoky() {
 
 	assert_eq!(t.malloc_size_of(), 1024);
 }
+
+#[test]
+fn derive_hashmap() {
+
+	use parity_util_mem::{MallocSizeOf, MallocSizeOfExt};
+
+	#[derive(MallocSizeOf, Default)]
+	struct Trivia {
+		hm: std::collections::HashMap<u64, Vec<u8>>,
+	}
+
+	let mut t = Trivia::default();
+
+	t.hm.insert(1, vec![0u8; 2048]);
+
+	assert_eq!(t.malloc_size_of(), 2088);
+}
