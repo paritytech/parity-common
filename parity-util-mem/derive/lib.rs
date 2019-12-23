45f833663b69b6e14aad99d7b10b82857bb9b7d4
NikVolf <nikvolf@gmail.com>
2019-12-23 22:11:50 +0300

    cargo fmt

--- parity-util-mem/derive/lib.rs
+++ parity-util-mem/derive/lib.rs
@@ -1,4 +1,3 @@
-
 //! A crate for deriving the MallocSizeOf trait.
 
 extern crate proc_macro2;
@@ -11,66 +10,56 @@ extern crate synstructure;
 decl_derive!([MallocSizeOf, attributes(ignore_malloc_size_of)] => malloc_size_of_derive);
 
 fn malloc_size_of_derive(s: synstructure::Structure) -> proc_macro2::TokenStream {
-    let match_body = s.each(|binding| {
-        let ignore = binding
-            .ast()
-            .attrs
-            .iter()
-            .any(|attr| match attr.parse_meta().unwrap() {
-                syn::Meta::Path(ref path) | syn::Meta::List(syn::MetaList { ref path, .. })
-                    if path.is_ident("ignore_malloc_size_of") =>
-                {
-                    panic!(
-                        "#[ignore_malloc_size_of] should have an explanation, \
-                         e.g. #[ignore_malloc_size_of = \"because reasons\"]"
-                    );
-                }
-                syn::Meta::NameValue(syn::MetaNameValue { ref path, .. })
-                    if path.is_ident("ignore_malloc_size_of") =>
-                {
-                    true
-                },
-                _ => false,
-            });
-        if ignore {
-            None
-        } else if let syn::Type::Array(..) = binding.ast().ty {
-            Some(quote! {
-                for item in #binding.iter() {
-                    sum += parity_util_mem::MallocSizeOf::size_of(item, ops);
-                }
-            })
-        } else {
-            Some(quote! {
-                sum += parity_util_mem::MallocSizeOf::size_of(#binding, ops);
-            })
-        }
-    });
+	let match_body = s.each(|binding| {
+		let ignore = binding.ast().attrs.iter().any(|attr| match attr.parse_meta().unwrap() {
+			syn::Meta::Path(ref path) | syn::Meta::List(syn::MetaList { ref path, .. })
+				if path.is_ident("ignore_malloc_size_of") =>
+			{
+				panic!(
+					"#[ignore_malloc_size_of] should have an explanation, \
+					 e.g. #[ignore_malloc_size_of = \"because reasons\"]"
+				);
+			}
+			syn::Meta::NameValue(syn::MetaNameValue { ref path, .. }) if path.is_ident("ignore_malloc_size_of") => true,
+			_ => false,
+		});
+		if ignore {
+			None
+		} else if let syn::Type::Array(..) = binding.ast().ty {
+			Some(quote! {
+				for item in #binding.iter() {
+					sum += parity_util_mem::MallocSizeOf::size_of(item, ops);
+				}
+			})
+		} else {
+			Some(quote! {
+				sum += parity_util_mem::MallocSizeOf::size_of(#binding, ops);
+			})
+		}
+	});
 
-    let ast = s.ast();
-    let name = &ast.ident;
-    let (impl_generics, ty_generics, where_clause) = ast.generics.split_for_impl();
-    let mut where_clause = where_clause.unwrap_or(&parse_quote!(where)).clone();
-    for param in ast.generics.type_params() {
-        let ident = &param.ident;
-        where_clause
-            .predicates
-            .push(parse_quote!(#ident: parity_util_mem::MallocSizeOf));
-    }
+	let ast = s.ast();
+	let name = &ast.ident;
+	let (impl_generics, ty_generics, where_clause) = ast.generics.split_for_impl();
+	let mut where_clause = where_clause.unwrap_or(&parse_quote!(where)).clone();
+	for param in ast.generics.type_params() {
+		let ident = &param.ident;
+		where_clause.predicates.push(parse_quote!(#ident: parity_util_mem::MallocSizeOf));
+	}
 
-    let tokens = quote! {
-        impl #impl_generics parity_util_mem::MallocSizeOf for #name #ty_generics #where_clause {
-            #[inline]
-            #[allow(unused_variables, unused_mut, unreachable_code)]
-            fn size_of(&self, ops: &mut parity_util_mem::MallocSizeOfOps) -> usize {
-                let mut sum = 0;
-                match *self {
-                    #match_body
-                }
-                sum
-            }
-        }
-    };
+	let tokens = quote! {
+		impl #impl_generics parity_util_mem::MallocSizeOf for #name #ty_generics #where_clause {
+			#[inline]
+			#[allow(unused_variables, unused_mut, unreachable_code)]
+			fn size_of(&self, ops: &mut parity_util_mem::MallocSizeOfOps) -> usize {
+				let mut sum = 0;
+				match *self {
+					#match_body
+				}
+				sum
+			}
+		}
+	};
 
-    tokens
+	tokens
 }
