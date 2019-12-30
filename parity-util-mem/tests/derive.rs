1d894fee2d3981fdc0fbe88da63209a62b6f4e24
NikVolf <nikvolf@gmail.com>
2019-12-30 21:51:03 +0300

    add ignore test

--- parity-util-mem/tests/derive.rs
+++ parity-util-mem/tests/derive.rs
@@ -1,8 +1,8 @@
+use parity_util_mem::{MallocSizeOf, MallocSizeOfExt};
+
 #[test]
 #[cfg(feature = "std")]
 fn derive_vec() {
-	use parity_util_mem::{MallocSizeOf, MallocSizeOfExt};
-
 	#[derive(MallocSizeOf)]
 	struct Trivia {
 		v: Vec<u8>,
@@ -16,8 +16,6 @@ fn derive_vec() {
 #[test]
 #[cfg(feature = "std")]
 fn derive_hashmap() {
-	use parity_util_mem::{MallocSizeOf, MallocSizeOfExt};
-
 	#[derive(MallocSizeOf, Default)]
 	struct Trivia {
 		hm: std::collections::HashMap<u64, Vec<u8>>,
@@ -28,4 +26,22 @@ fn derive_hashmap() {
 	t.hm.insert(1, vec![0u8; 2048]);
 
 	assert!(t.malloc_size_of() > 2000);
+}
+
+#[test]
+#[cfg(feature = "std")]
+fn derive_ignore() {
+	#[derive(MallocSizeOf, Default)]
+	struct Trivia {
+		hm: std::collections::HashMap<u64, Vec<u8>>,
+		#[ignore_malloc_size_of = "I don't like vectors"]
+		v: Vec<u8>,
+	}
+
+	let mut t = Trivia::default();
+
+	t.hm.insert(1, vec![0u8; 2048]);
+	t.v = vec![0u8; 1024];
+
+	assert!(t.malloc_size_of() < 3000);
 }
