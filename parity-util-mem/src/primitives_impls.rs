2e1279fb5572cb88fa6b79d602e6458376642ae9
NikVolf <nikvolf@gmail.com>
2020-02-03 12:39:13 +0300

    split off primitives

--- parity-util-mem/src/primitives_impls.rs
+++ parity-util-mem/src/primitives_impls.rs
@@ -1,4 +1,4 @@
-// Copyright 2015-2019 Parity Technologies (UK) Ltd.
+// Copyright 2020 Parity Technologies (UK) Ltd.
 // This file is part of Parity.
 
 // Parity is free software: you can redistribute it and/or modify
@@ -14,9 +14,21 @@
 // You should have received a copy of the GNU General Public License
 // along with Parity.  If not, see <http://www.gnu.org/licenses/>.
 
-//! Implementation of `MallocSize` for common ethereum types: fixed hashes
-//! and uints.
+//! Implementation of `MallocSize` primitive types.
 
-use ethereum_types::{Bloom, H128, H264, H32, H520, H64, U64};
+use primitive_types::{H160, H256, H512, U128, U256, U512};
 
-malloc_size_of_is_0!(U64, H32, H64, H128, H264, H520, Bloom);
+malloc_size_of_is_0!(U128, U256, U512, H160, H256, H512);
+
+#[cfg(test)]
+mod tests {
+
+	use primitive_types::H256;
+
+	#[test]
+	fn smoky() {
+		let v = vec![H256::zero(), H256::zero()];
+
+		assert!(crate::MallocSizeOfExt::malloc_size_of(&v) >= 64);
+	}
+}
