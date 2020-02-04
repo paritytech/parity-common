06ae74e64aa61633a67afb5c16ed943ffd4d9542
Andronik Ordian <write@reusable.software>
2020-02-04 14:27:33 +0100

    remove libc feature from fixed-hash (#317)
    * fixed-hash: rename impl_libc to impl_cmp
    
    * fixed-hash: add a cmp bench
    
    * fied-hash: try cmp without libc
    
    * cargo fmt
    
    * update CHANGELOGs
    
    * fixed-hash: remove unused type in the bench

--- fixed-hash/src/hash.rs
+++ fixed-hash/src/hash.rs
@@ -313,7 +313,7 @@ macro_rules! construct_fixed_hash {
 
 		impl_byteorder_for_fixed_hash!($name);
 		impl_rand_for_fixed_hash!($name);
-		impl_libc_for_fixed_hash!($name);
+		impl_cmp_for_fixed_hash!($name);
 		impl_rustc_hex_for_fixed_hash!($name);
 		impl_quickcheck_for_fixed_hash!($name);
 	}
@@ -527,17 +527,9 @@ macro_rules! impl_rand_for_fixed_hash {
 	};
 }
 
-// Implementation for disabled libc crate support.
-//
-// # Note
-//
-// Feature guarded macro definitions instead of feature guarded impl blocks
-// to work around the problems of introducing `libc` crate feature in
-// a user crate.
-#[cfg(not(all(feature = "libc", not(target_os = "unknown"))))]
 #[macro_export]
 #[doc(hidden)]
-macro_rules! impl_libc_for_fixed_hash {
+macro_rules! impl_cmp_for_fixed_hash {
 	( $name:ident ) => {
 		impl $crate::core_::cmp::PartialEq for $name {
 			#[inline]
@@ -550,52 +542,6 @@ macro_rules! impl_libc_for_fixed_hash {
 			#[inline]
 			fn cmp(&self, other: &Self) -> $crate::core_::cmp::Ordering {
 				self.as_bytes().cmp(other.as_bytes())
-			}
-		}
-	};
-}
-
-// Implementation for enabled libc crate support.
-//
-// # Note
-//
-// Feature guarded macro definitions instead of feature guarded impl blocks
-// to work around the problems of introducing `libc` crate feature in
-// a user crate.
-#[cfg(all(feature = "libc", not(target_os = "unknown")))]
-#[macro_export]
-#[doc(hidden)]
-macro_rules! impl_libc_for_fixed_hash {
-	( $name:ident ) => {
-		impl $crate::core_::cmp::PartialEq for $name {
-			#[inline]
-			fn eq(&self, other: &Self) -> bool {
-				unsafe {
-					$crate::libc::memcmp(
-						self.as_ptr() as *const $crate::libc::c_void,
-						other.as_ptr() as *const $crate::libc::c_void,
-						Self::len_bytes(),
-					) == 0
-				}
-			}
-		}
-
-		impl $crate::core_::cmp::Ord for $name {
-			fn cmp(&self, other: &Self) -> $crate::core_::cmp::Ordering {
-				let r = unsafe {
-					$crate::libc::memcmp(
-						self.as_ptr() as *const $crate::libc::c_void,
-						other.as_ptr() as *const $crate::libc::c_void,
-						Self::len_bytes(),
-					)
-				};
-				if r < 0 {
-					return $crate::core_::cmp::Ordering::Less;
-				}
-				if r > 0 {
-					return $crate::core_::cmp::Ordering::Greater;
-				}
-				$crate::core_::cmp::Ordering::Equal
 			}
 		}
 	};
