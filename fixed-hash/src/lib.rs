dd38f81117a15382c95ea7318f694f68583a9481
Andronik Ordian <write@reusable.software>
2020-02-10 16:53:21 +0100

    Revert "Remove libc completely (#333)"
    This reverts commit 996d3703489b574946ff22b513d6f15f1d747808.

--- fixed-hash/src/lib.rs
+++ fixed-hash/src/lib.rs
@@ -18,6 +18,10 @@ pub extern crate alloc as alloc_;
 #[doc(hidden)]
 pub use core as core_;
 
+#[cfg(all(feature = "libc", not(target_os = "unknown")))]
+#[doc(hidden)]
+pub use libc;
+
 // This disables a warning for unused #[macro_use(..)]
 // which is incorrect since the compiler does not check
 // for all available configurations.
@@ -33,6 +37,10 @@ pub use static_assertions::const_assert;
 #[cfg(feature = "byteorder")]
 #[doc(hidden)]
 pub use byteorder;
+
+#[cfg(not(feature = "libc"))]
+#[doc(hidden)]
+pub mod libc {}
 
 #[cfg(feature = "rustc-hex")]
 #[doc(hidden)]
