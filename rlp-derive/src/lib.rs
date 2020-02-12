2a7c2f82a5511fd4ea795e408fa61b8c5fe90eb4
Andronik Ordian <write@reusable.software>
2020-02-12 15:58:53 +0100

    rlp-derive: more module docs

--- rlp-derive/src/lib.rs
+++ rlp-derive/src/lib.rs
@@ -7,6 +7,14 @@
 // except according to those terms.
 
 //! Derive macro for `#[derive(RlpEncodable, RlpDecodable)]`.
+//!
+//! For example of usage see `./tests/rlp.rs`.
+//!
+//! This library also supports up to 1 `#[rlp(default)]` in a struct,
+//! which is similar to [`#[serde(default)]`](https://serde.rs/field-attrs.html#default)
+//! with the caveat that we use the `Default` value if
+//! the field deserialization fails, as we don't serialize field
+//! names and there is no way to tell if it is present or not.
 
 extern crate proc_macro;
 
