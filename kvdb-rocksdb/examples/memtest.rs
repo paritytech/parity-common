41854f42ae8041651bb236a2b164227f2a66d86e
NikVolf <nikvolf@gmail.com>
2020-03-16 16:18:28 +0300

    address review

--- kvdb-rocksdb/examples/memtest.rs
+++ kvdb-rocksdb/examples/memtest.rs
@@ -1,3 +1,22 @@
+// Copyright 2020 Parity Technologies (UK) Ltd.
+// This file is part of Parity Ethereum.
+
+// Parity Ethereum is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+
+// Parity Ethereum is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License
+// along with Parity Ethereum.  If not, see <http://www.gnu.org/licenses/>.
+
+// This program starts writing random data to the database with 100 (COLUMN_COUNT)
+// columns and never stops until interrapted.
+
 use ethereum_types::H256;
 use keccak_hash::keccak;
 use kvdb_rocksdb::{Database, DatabaseConfig};
