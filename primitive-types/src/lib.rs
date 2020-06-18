c3d6be5a2efa2bff7095c18fbf52e666fb7ed2cd
Andronik Ordian <write@reusable.software>
2020-06-18 14:56:04 +0200

    primitive-types: update scale-info to 0.2

--- primitive-types/src/lib.rs
+++ primitive-types/src/lib.rs
@@ -17,7 +17,7 @@
 use core::convert::TryFrom;
 use fixed_hash::{construct_fixed_hash, impl_fixed_hash_conversions};
 #[cfg(feature = "scale-info")]
-use scale_info::Metadata;
+use scale_info::TypeInfo;
 use uint::{construct_uint, uint_full_mul_reg};
 
 /// Error type for conversion.
@@ -29,33 +29,33 @@ pub enum Error {
 
 construct_uint! {
 	/// 128-bit unsigned integer.
-	#[cfg_attr(feature = "scale-info", derive(Metadata))]
+	#[cfg_attr(feature = "scale-info", derive(TypeInfo))]
 	pub struct U128(2);
 }
 construct_uint! {
 	/// 256-bit unsigned integer.
-	#[cfg_attr(feature = "scale-info", derive(Metadata))]
+	#[cfg_attr(feature = "scale-info", derive(TypeInfo))]
 	pub struct U256(4);
 }
 construct_uint! {
 	/// 512-bits unsigned integer.
-	#[cfg_attr(feature = "scale-info", derive(Metadata))]
+	#[cfg_attr(feature = "scale-info", derive(TypeInfo))]
 	pub struct U512(8);
 }
 
 construct_fixed_hash! {
 	/// Fixed-size uninterpreted hash type with 20 bytes (160 bits) size.
-	#[cfg_attr(feature = "scale-info", derive(Metadata))]
+	#[cfg_attr(feature = "scale-info", derive(TypeInfo))]
 	pub struct H160(20);
 }
 construct_fixed_hash! {
 	/// Fixed-size uninterpreted hash type with 32 bytes (256 bits) size.
-	#[cfg_attr(feature = "scale-info", derive(Metadata))]
+	#[cfg_attr(feature = "scale-info", derive(TypeInfo))]
 	pub struct H256(32);
 }
 construct_fixed_hash! {
 	/// Fixed-size uninterpreted hash type with 64 bytes (512 bits) size.
-	#[cfg_attr(feature = "scale-info", derive(Metadata))]
+	#[cfg_attr(feature = "scale-info", derive(TypeInfo))]
 	pub struct H512(64);
 }
 
