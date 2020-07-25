4a6278ed9d2279f96a6b744abba34e38260bc26a
Andronik Ordian <write@reusable.software>
2020-07-25 21:43:07 +0200

    fix benches in uint and plain_hasher

--- plain_hasher/benches/bench.rs
+++ plain_hasher/benches/bench.rs
@@ -9,14 +9,14 @@
 use std::collections::hash_map::DefaultHasher;
 use std::hash::Hasher;
 
-use criterion::{criterion_group, criterion_main, Criterion};
+use criterion::{black_box, criterion_group, criterion_main, Criterion};
 use plain_hasher::PlainHasher;
 
 fn bench_write_hasher(c: &mut Criterion) {
 	c.bench_function("write_plain_hasher", |b| {
 		b.iter(|| {
 			(0..100u8).fold(PlainHasher::default(), |mut old, new| {
-				let bb = [new; 32];
+				let bb = black_box([new; 32]);
 				old.write(&bb);
 				old
 			});
@@ -25,7 +25,7 @@ fn bench_write_hasher(c: &mut Criterion) {
 	c.bench_function("write_default_hasher", |b| {
 		b.iter(|| {
 			(0..100u8).fold(DefaultHasher::default(), |mut old, new| {
-				let bb = [new; 32];
+				let bb = black_box([new; 32]);
 				old.write(&bb);
 				old
 			});
