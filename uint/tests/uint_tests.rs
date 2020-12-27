10e97d247342e6ea197114e893ed62f20f373255
Georgios Konstantopoulos <me@gakonst.com>
2020-12-28 00:57:16 +0200

    fix(uint): make from_str parse 0x-prefixed strings

--- uint/tests/uint_tests.rs
+++ uint/tests/uint_tests.rs
@@ -132,7 +132,9 @@ fn uint256_from() {
 	);
 
 	// test initializtion from string
+	let sa2 = U256::from_str("0x0a").unwrap();
 	let sa = U256::from_str("0a").unwrap();
+	assert_eq!(sa2, sa);
 	assert_eq!(e, sa);
 	assert_eq!(U256([0, 0, 0, 0]), U256::from_str("").unwrap());
 	assert_eq!(U256([0x1, 0, 0, 0]), U256::from_str("1").unwrap());
