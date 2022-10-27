26c1423bacac3c248c45ea058cdee213d23adfb3
drskalman <35698397+drskalman@users.noreply.github.com>
2022-10-27 09:37:38 +0000

    add `H384` and `H768` hash types (#684)
    * add H384 and H768 hash types
    
    * implement `impl-serde`, `impl-codec`, `impl-rlp` for `H384` and `H768` types.
    
    * remove redundant double impl of serde for H384

--- primitive-types/src/lib.rs
+++ primitive-types/src/lib.rs
@@ -63,9 +63,19 @@ construct_fixed_hash! {
 	pub struct H256(32);
 }
 construct_fixed_hash! {
+	/// Fixed-size uninterpreted hash type with 48 bytes (384 bits) size.
+	#[cfg_attr(feature = "scale-info", derive(TypeInfo))]
+	pub struct H384(48);
+}
+construct_fixed_hash! {
 	/// Fixed-size uninterpreted hash type with 64 bytes (512 bits) size.
 	#[cfg_attr(feature = "scale-info", derive(TypeInfo))]
 	pub struct H512(64);
+}
+construct_fixed_hash! {
+	/// Fixed-size uninterpreted hash type with 96 bytes (768 bits) size.
+	#[cfg_attr(feature = "scale-info", derive(TypeInfo))]
+	pub struct H768(96);
 }
 
 #[cfg(feature = "num-traits")]
@@ -90,7 +100,9 @@ mod serde {
 	impl_fixed_hash_serde!(H128, 16);
 	impl_fixed_hash_serde!(H160, 20);
 	impl_fixed_hash_serde!(H256, 32);
+	impl_fixed_hash_serde!(H384, 48);
 	impl_fixed_hash_serde!(H512, 64);
+	impl_fixed_hash_serde!(H768, 96);
 }
 
 #[cfg(feature = "impl-codec")]
@@ -105,7 +117,9 @@ mod codec {
 	impl_fixed_hash_codec!(H128, 16);
 	impl_fixed_hash_codec!(H160, 20);
 	impl_fixed_hash_codec!(H256, 32);
+	impl_fixed_hash_codec!(H384, 48);
 	impl_fixed_hash_codec!(H512, 64);
+	impl_fixed_hash_codec!(H768, 96);
 }
 
 #[cfg(feature = "impl-rlp")]
@@ -120,7 +134,9 @@ mod rlp {
 	impl_fixed_hash_rlp!(H128, 16);
 	impl_fixed_hash_rlp!(H160, 20);
 	impl_fixed_hash_rlp!(H256, 32);
+	impl_fixed_hash_rlp!(H384, 48);
 	impl_fixed_hash_rlp!(H512, 64);
+	impl_fixed_hash_rlp!(H768, 96);
 }
 
 impl_fixed_hash_conversions!(H256, H160);
