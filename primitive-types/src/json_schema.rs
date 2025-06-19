3c66c03835fdc4daf2d1ddbb0b800979191f3cf7
Bastian Köcher <git@kchr.de>
2025-06-19 17:37:41 +0200

    primitive-types: Fix json schema (#916)
    * primitive-types: Fix json schema
    
    Apparently the latest version broke it and thus, CI is failing for other prs.
    
    * Rustfmt

--- primitive-types/src/json_schema.rs
+++ primitive-types/src/json_schema.rs
@@ -1,35 +1,37 @@
 use super::*;
 #[cfg(not(feature = "std"))]
 use alloc::{
-	borrow::ToOwned,
+	borrow::{Cow, ToOwned},
 	string::{String, ToString},
 };
+#[cfg(feature = "std")]
+use std::borrow::Cow;
 
-use schemars::{gen::SchemaGenerator, schema::Schema, JsonSchema};
+use schemars::{json_schema, JsonSchema, Schema, SchemaGenerator};
 
 impl JsonSchema for H160 {
-	fn schema_name() -> String {
-		"HexEncoded20Bytes".to_owned()
+	fn schema_name() -> Cow<'static, str> {
+		"HexEncoded20Bytes".into()
 	}
 
-	fn json_schema(gen: &mut SchemaGenerator) -> Schema {
-		let mut schema = gen.subschema_for::<String>().into_object();
-		schema.metadata().description = Some("Hex encoded 20 bytes".to_string());
-		schema.string().pattern = Some("^0(x|X)[a-fA-F0-9]{40}$".to_string());
-		schema.into()
+	fn json_schema(_: &mut SchemaGenerator) -> Schema {
+		json_schema!({
+			"description": "Hex encoded 20 bytes",
+			"pattern": "^0(x|X)[a-fA-F0-9]{40}$",
+		})
 	}
 }
 
 impl JsonSchema for U256 {
-	fn schema_name() -> String {
-		"U256String".to_string()
+	fn schema_name() -> Cow<'static, str> {
+		"U256String".into()
 	}
 
-	fn json_schema(gen: &mut SchemaGenerator) -> Schema {
-		let mut schema = gen.subschema_for::<String>().into_object();
-		schema.metadata().description = Some("256-bit Unsigned Integer".to_string());
-		schema.string().pattern = Some("^(0|[1-9][0-9]{0,77})$".to_string());
-		schema.into()
+	fn json_schema(_: &mut SchemaGenerator) -> Schema {
+		json_schema!({
+			"description": "256-bit Unsigned Integer",
+			"pattern": "^(0|[1-9][0-9]{0,77})$",
+		})
 	}
 }
 
@@ -44,14 +46,17 @@ mod tests {
 
 	#[test]
 	fn hex_encoded_20_bytes() {
-		let schema = H160::json_schema(&mut schemars::gen::SchemaGenerator::default());
+		let schema = H160::json_schema(&mut schemars::SchemaGenerator::default());
 		let schema_json = serde_json::to_value(&schema).unwrap();
 		let schema = jsonschema::Validator::options()
 			.with_draft(Draft::Draft7)
 			.build(&schema_json)
 			.unwrap();
 		let value = serde_json::to_value("0x55086adeca661185c437d92b9818e6eda6d0d047").unwrap();
-		assert!(schema.validate(&value).is_ok());
+		schema
+			.validate(&value)
+			.map_err(|e| e.into_iter().map(|e| e.to_string()).collect::<Vec<_>>())
+			.unwrap();
 		let value = serde_json::to_value("0X0E9C8DA9FD4BDD3281879D9E328D8D74D02558CC").unwrap();
 		assert!(schema.validate(&value).is_ok());
 
@@ -61,7 +66,7 @@ mod tests {
 
 	#[test]
 	fn u256() {
-		let schema = U256::json_schema(&mut schemars::gen::SchemaGenerator::default());
+		let schema = U256::json_schema(&mut schemars::SchemaGenerator::default());
 		let schema_json = serde_json::to_value(&schema).unwrap();
 		let schema = jsonschema::Validator::options()
 			.with_draft(Draft::Draft7)
